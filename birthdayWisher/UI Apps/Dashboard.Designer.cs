03fb63bc0e4d007f7068ddef2776efc2ec04a9cc
detrujaanil <detrujaanil@gmail.com>
2015-11-02 20:59:05 +0530

    Devline

---
+++ birthdayWisher/UI Apps/Dashboard.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace birthdayWisher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileControl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileControl
+            // 
+            this.fileControl.Location = new System.Drawing.Point(12, 12);
+            this.fileControl.Name = "fileControl";
+            this.fileControl.Size = new System.Drawing.Size(103, 31);
+            this.fileControl.TabIndex = 0;
+            this.fileControl.Text = "Import Data";
+            this.fileControl.UseVisualStyleBackColor = true;
+            this.fileControl.Click += new System.EventHandler(this.fileControl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(380, 255);
+            this.Controls.Add(this.fileControl);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "birthdayWisher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileControl;
+
+    }
+}
+
